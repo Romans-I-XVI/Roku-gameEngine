Romans-I-XVI/Roku-gameEngine

commit c92aefe19ca7a90a20d8e1c6fd0363c637c29287

Improved performance of postGameEvent

gameEngine.brs
@@ -1457,15 +1457,10 @@ function new_game(canvas_width, canvas_height, canvas_as_screen_if_possible = fa
 
 	' ############### postGameEvent() function - Begin ###############
 	game.postGameEvent = function(event as string, data = {} as object) as void
-		object_keys = m.Instances.Keys()
-		for each object_key in object_keys
-			instance_keys = m.Instances[object_key].Keys()
-			for each instance_key in instance_keys
-				instance = m.Instances[object_key][instance_key]
-				if instance <> invalid and instance.id <> invalid and instance.onGameEvent <> invalid
-					instance.onGameEvent(event, data)
-				end if
-			end for
+		for each instance in m.sorted_instances
+			if instance <> invalid and instance.id <> invalid and instance.onGameEvent <> invalid
+				instance.onGameEvent(event, data)
+			end if
 		end for
 	end function
 	' ############### postGameEvent() function - End ###############

commit 9ec32ae9897bf1ccf36f7ab9985785fec8437ead

Fixed bug with recursive postGameEvent calls introduced in c92aefe19ca7a90a20d8e1c6fd0363c637c29287 When for each is used any postGameEvent calls that occur within onGameEvent would cause the previous for each to get cut short and not finish calling onGameEvent on remaining entities with the original event. This I believe is because for each uses ifEnum internally and gets reset when a new for each begins, meaning the old loop position is forgotten. Solved by simply switching to for loop

gameEngine.brs
@@ -1457,7 +1457,8 @@ function new_game(canvas_width, canvas_height, canvas_as_screen_if_possible = fa
 
 	' ############### postGameEvent() function - Begin ###############
 	game.postGameEvent = function(event as string, data = {} as object) as void
-		for each instance in m.sorted_instances
+		for i = 0 to m.sorted_instances.Count() - 1
+			instance = m.sorted_instances[i]
 			if instance <> invalid and instance.id <> invalid and instance.onGameEvent <> invalid
 				instance.onGameEvent(event, data)
 			end if